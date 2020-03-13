e3cb55727c910508dcd3d5e5ac842fea732991d1
Tharindu <tharindunuwa222GMAIL.COM>
2020-03-13 12:49:54 +0530

    my first commit

---
+++ src/main/java/lk/demo/artifi/ArtifiApplication.java
@@ -0,0 +1,13 @@
+package lk.demo.artifi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/demo/artifi/ArtifiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.demo.artifi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
